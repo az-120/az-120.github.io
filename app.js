99ad5b7e45eb7974dcd29d629c90a423bee4240f
Arthur Zhao <adz6@duke.edu>
2023-11-21 15:38:27 -0500

    developments

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-function alertButton() {
-    alert("Hello")
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+
+    const toggleButton = document.getElementById('toggleModeButton');
+    const isDarkMode = body.classList.contains('dark-mode');
+
+    toggleButton.textContent = isDarkMode ? 'Toggle Light Mode' : 'Toggle Dark Mode';
 }
