60228e8b4ad092bb8f10f8a31b297d8acb058d3a
Arthur Zhao <adz6@duke.edu>
2025-01-05 18:16:44 -0500

    created up to template for projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import styles from "./ProjectsStyles.module.css";
+import viberr from "../../assets/viberr.png";
+import freshBurger from "../../assets/fresh-burger.png";
+import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/az-120/portfolio-website"
+          h3="Portfolio Website"
+          p="Personal Portfolio Website Code Link"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/az-120/portfolio-website"
+          h3="Fresh Burger"
+          p="Burger App"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
