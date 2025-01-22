3791279b773c72700c96b6791f130bd7fe6afa99
Arthur Zhao <adz6@duke.edu>
2025-01-21 19:44:13 -0500

    added mail icon

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -4,8 +4,8 @@ import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/hero-img.png";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
-import twitterLight from "../../assets/twitter-light.svg";
-import twitterDark from "../../assets/twitter-dark.svg";
+import mailLight from "../../assets/mail-dark.svg";
+import mailDark from "../../assets/mail-light.svg";
 import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
@@ -16,7 +16,7 @@ function Hero() {
   const {theme, toggleTheme} = useTheme();
 
   const themeIcon = theme === "light" ? sun : moon;
-  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const mailIcon = theme === "light" ? mailLight : mailDark;
   const githubIcon = theme === "light" ? githubLight : githubDark;
   const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
 
@@ -64,11 +64,11 @@ function Hero() {
           <a href="https://linkedin.com/in/arthurdezhao" target="_blank">
             <img src={linkedinIcon} alt="LinkedIn Icon" />
           </a>
-          <a href="https://instagram.com/arthvrzhao" target="_blank">
-            <img src={twitterIcon} alt="Twitter Icon" />
-          </a>
           <a href="https://github.com/az-120" target="_blank">
-            <img src={githubIcon} alt="Gwitter Icon" />
+            <img src={githubIcon} alt="GitHub Icon" />
+          </a>
+          <a href="mailto:arthurdezhao@gmail.com" target="_blank">
+            <img src={mailIcon} alt="Mail Icon" />
           </a>
         </span>
       </div>
