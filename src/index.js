7c43e7f3a038c24df14cad815b0473a171b69a3c
Arthur Zhao <adz6@duke.edu>
2025-06-28 14:05:04 -0700

    npm deploy to access site

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import {ThemeProvider} from "./common/ThemeContext.jsx";
+import { ThemeProvider } from "./common/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
