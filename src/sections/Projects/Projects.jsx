085f23782c49671c88fe4c8b56c61b1188b9abaf
Arthur Zhao <adz6@duke.edu>
2025-06-30 21:30:35 -0700

    projects made, just added 3 for now

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,24 +1,35 @@
 import styles from "./ProjectsStyles.module.css";
-import viberr from "../../assets/viberr.png";
-import freshBurger from "../../assets/fresh-burger.png";
-import ProjectCard from "../../common/ProjectCard";
+import TiltedCard from './TiltedCard';
+
+import personal_website from "../../assets/personal-website.png"
+import LLM_ASR from "../../assets/LLM-ASR.png"
+import hate_speech from "../../assets/hatespeech.png"
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard
-          src={viberr}
-          link="https://github.com/az-120/portfolio-website"
-          h3="Portfolio Website"
-          p="Personal Portfolio Website Code Link"
+        <TiltedCard
+          imageSrc={personal_website}
+          altText="Personal Website"
+          captionText="Personal Website"
+          displayOverlayContent={true}
+          link='https://github.com/az-120/az-120.github.io'
         />
-        <ProjectCard
-          src={freshBurger}
-          link="https://github.com/az-120/portfolio-website"
-          h3="Fresh Burger"
-          p="Burger App"
+        <TiltedCard
+          imageSrc={LLM_ASR}
+          altText="Improving ASR with LLM on accented speech"
+          captionText="Improving ASR with LLM on accented speech"
+          displayOverlayContent={true}
+          link='https://github.com/az-120/LLM-ASR'
+        />
+        <TiltedCard
+          imageSrc={hate_speech}
+          altText="ML Hate Speech Detector"
+          captionText="ML Hate Speech Detector"
+          displayOverlayContent={true}
+          link='https://github.com/az-120/HateSpeechDetector/tree/main'
         />
       </div>
     </section>
@@ -26,6 +37,3 @@ function Projects() {
 }
 
 export default Projects;
-
-
-// Circular Gallery
