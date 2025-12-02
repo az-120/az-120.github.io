a3a7bf91f18867317cbc0a78f95ddf5bdaac63f7
Arthur Zhao <adz6@duke.edu>
2025-12-02 13:18:56 -0500

    add beattorrent project, slight css adjustments

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import styles from "./ProjectsStyles.module.css";
 import TiltedCard from "./TiltedCard";
 
 import personal_website from "../../assets/personal-website.png";
+import BeatTorrent from "../../assets/beat-torrent.png";
 import LLM_ASR from "../../assets/LLM-ASR.png";
 import hate_speech from "../../assets/hatespeech.png";
 
@@ -18,11 +19,11 @@ function Projects() {
           link="https://github.com/az-120/az-120.github.io"
         />
         <TiltedCard
-          imageSrc={hate_speech}
-          altText="ML Hate Speech Detector"
-          captionText="ML Hate Speech Detector"
+          imageSrc={BeatTorrent}
+          altText="Distributed P2P Synchronized Music Streaming Player"
+          captionText="Distributed P2P Synchronized Music Streaming Player"
           displayOverlayContent={true}
-          link="https://github.com/az-120/HateSpeechDetector/tree/main"
+          link="https://github.com/az-120/beat-torrent"
         />
         <TiltedCard
           imageSrc={LLM_ASR}
@@ -30,6 +31,13 @@ function Projects() {
           captionText="Improving ASR with LLM on accented speech"
           displayOverlayContent={true}
           link="https://github.com/az-120/LLM-ASR"
+        />
+        <TiltedCard
+          imageSrc={hate_speech}
+          altText="ML Hate Speech Detector"
+          captionText="ML Hate Speech Detector"
+          displayOverlayContent={true}
+          link="https://github.com/az-120/HateSpeechDetector/tree/main"
         />
       </div>
     </section>
